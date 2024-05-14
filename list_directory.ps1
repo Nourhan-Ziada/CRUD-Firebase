b97606bc54ca2fc6e87f05005bd6b5b2226bd83f
Nourhan Al-Sayed Salama <90463182+Nourhan-Ziada@users.noreply.github.com>
2024-05-14 17:40:26 +0300

    Update list_directory.ps1

--- list_directory.ps1
+++ list_directory.ps1
@@ -1,3 +1,5 @@
 Write-Host "Executing list_directory.ps1 script..."
+Set-Location "G:\FCI\Fourth_Year\FCAI-CS-Final-Semester\Cloud Computing\Tasks\CRUD-Firebase"
+
 Write-Host "Current directory: $(Get-Location)"
-Get-ChildItem
+ls
